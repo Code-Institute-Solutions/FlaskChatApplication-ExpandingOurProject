e595ef584f45ec6084da314c0c962b1a66f0ef60
NielMc <nielmce@gmail.com>
2018-02-21 14:20:31 +0000

    added missing import

--- 04-creating_users/run.py
+++ 04-creating_users/run.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 messages = []
@@ -10,11 +10,11 @@ def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(message_dict)
+    messages.append(messages_dict)
 
 
 def get_all_messages():
-    """Get al of the messages and separate them by a `br`"""
+    """Get all of the messages and separate them by a `br`"""
     return messages
 
 
@@ -24,7 +24,7 @@ def index():
     if request.method == "POST":
         with open("data/users.txt", "a") as user_list:
             user_list.writelines(request.form["username"] + "\n")
-        return redirect(username)
+        return redirect(request.form["username"])
     return render_template("index.html")
 
 
@@ -34,10 +34,11 @@ def user(username):
     return "<h1>Welcome, {0}</h1> {1}".format(username, messages)
 
 
-@app.route('/<username/<message>')
+@app.route('/<username>/<message>')
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect(username)
+
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
